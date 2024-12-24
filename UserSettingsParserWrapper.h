114862097b76f4e66c6f11ed6ceba8ddd3de64d4
gokul <115553501+gokul-hub-dev@users.noreply.github.com>
2024-12-24 15:40:50 +0530

    Create UserSettingsParserWrapper.h

---
+++ UserSettingsParserWrapper.h
@@ -0,0 +1,25 @@
+#ifndef USER_SETTINGS_PARSER_WRAPPER_H
+#define USER_SETTINGS_PARSER_WRAPPER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void create_file_if_not_exists(const char* filename);
+void load_settings(const char* filename);
+const char* get_value(const char* key);
+int get_value_as_int(const char* key);
+double get_value_as_double(const char* key);
+int get_value_as_bool(const char* key);
+void set_value(const char* key, const char* value);
+void set_value_as_int(const char* key, int value);
+void set_value_as_double(const char* key, double value);
+void set_value_as_bool(const char* key, int value);
+void save_settings();
+void save_settings_as(const char* filename);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // USER_SETTINGS_PARSER_WRAPPER_H
