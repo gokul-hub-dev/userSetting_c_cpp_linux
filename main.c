d578fc2cd9bd77964f3d2500f3277f56abfba3be
gokul <115553501+gokul-hub-dev@users.noreply.github.com>
2024-12-24 15:43:13 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "UserSettingsParserWrapper.h"
+#include <stdio.h>
+
+int main() {
+    const char* filename = "settings.txt";
+
+    create_file_if_not_exists(filename);
+    load_settings(filename);
+
+    set_value("username", "gokul");
+    set_value_as_int("age", 30);
+    set_value_as_bool("is_admin", 1);
+
+    printf("Username: %s\n", get_value("username"));
+    printf("Age: %d\n", get_value_as_int("age"));
+    printf("Is Admin: %d\n", get_value_as_bool("is_admin"));
+
+    save_settings();
+
+    return 0;
+}
+
