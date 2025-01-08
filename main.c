be0c65ae10718f0d9a0d1659b9d3406472c5c017
gokul <115553501+gokul-hub-dev@users.noreply.github.com>
2025-01-08 19:03:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,34 @@
 #include "UserSettingsParserWrapper.h"
 #include <stdio.h>
-
+#include <stdbool.h>
 int main() {
     const char* filename = "settings.txt";
 
-    create_file_if_not_exists(filename);
-    load_settings(filename);
-
-    set_value("username", "gokul");
-    set_value_as_int("age", 30);
-    set_value_as_bool("is_admin", 1);
-
+    //create_file_if_not_exists(filename);
+    int ret=load_settings(filename);
+    if(ret==-1)
+    {
+        printf("Error loading settings\n");
+        return -1;
+    }
+    //set_value("username", "gokul");
+    //set_value_as_int("age", 30);
+    //set_value_as_bool("is_admin", 1);
+    bool ret1=0;
+    ret1=get_value_as_bool("status");
+    if(ret1==1)
+    {
+        printf("SUCCESS\n");
+    }
+    else
+    {
+        printf("FAILURE\n");
+    }
     printf("Username: %s\n", get_value("username"));
     printf("Age: %d\n", get_value_as_int("age"));
-    printf("Is Admin: %d\n", get_value_as_bool("is_admin"));
+    printf("status: %d\n", get_value_as_bool("status"));
 
     save_settings();
-    printf("user config : %s\n", get_usr_config_path());
-    printf("wifi config : %s\n", get_wifi_config_path());
 
     return 0;
 }
